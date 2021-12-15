e2c4c9c41604e6df626de7d1109d026f836fe0ea
ElliotEriksson <elliot@serait.se>
2021-12-15 16:22:01 +0100

    Second commit, improved code, added naughtylist function and readme

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # Substitute-Santa
+
+## Skapare
+
+Elliot Eriksson
+
+## Hjälp
+
+Viktor Dekker och Kuba Kijak med print_slow funktion
+
+## Projekt
+
+Tomtevikariat
+Ni ska skapa ert första github-projekt!
+
+Gå in på github och skapa ett nytt repository ni kallar för "Substitute Santa".
+Lägg till en README och en gitignore. gitignoren ska innehålla .txt (dvs ni kommer inte att ladda upp eller ner .txt-filer)
+
+Projektet ni ska jobba med denna vecka är följande.
+Tomten är sjuk och ni ska avlasta honom.
+
+Ditt program handlar om önskelistor.
+Skapa ett program som har en meny.
+Menyn ska ha två lägen.
+1. Skapa önskelista.
+2. Läs upp önskelista.
+
+Skapa Önskelista
+När du skapar en önskelista så behöver du be användaren om några saker.
+1. Vad ska filen heta?
+2. Vad heter barnet?
+3. Vilka saker ska skrivas till på önskelistan? (for- eller while-loop)
+
+Gör sedan så att du skriver ner i filen på följande sätt.
+Rad 1: Namn på barnet
+Rad 2 till n: Sak
+
+Läs upp önskelista
+Fråga användaren om vad filen heter.
+Skriv sedan ut namnet på barnet (första raden).
+Skriv sedan "ÖNSKELISTA"
+Skriv sedan ut rad för rad vad önskelistan innehåller.
+
+Lägg till en naughtylist.
+Antag att du har en lista redan som heter "kolbarn". Filen innehåller bara rader med namn och inget  annat.
+Gör liknande funktioner gällande detta.
+
+1. Läs från kolbarn.
+2. Lägg till på kolbarn. Innan du skriver till kolbarn så ska du få se barnen på listan först så du inte råkar lägga till samma sen tidigare.
+
+## Funktioner
+
+### Print_slow
+
+printar ut text långsamt för att det ska se kollt ut.
+
+### Wishlist
+
+En av main funktionerna som innehåller allting som har att göra med själva önskelistan där du ska antingen skriva en ny eller läsa upp
+
+### Naughty List
+
+Den andra main funktionen som innehåller allting som har att göra med kolbarnslistan där man kan lägga till eller läsa upp.
+
+## Vad jag har lärt mig
+
+Jag har blivit mycket bättre på att hantera filer, och lärde mig hur många skriver ut filer utan att ha med linebreaks.
--- Wishlist.py
+++ Wishlist.py
@@ -1,25 +1,37 @@
 #Elliot Eriksson
+#TEINF-20
 #14/12-2021
-#Wishlist
+#Wishlist & Naughtylist
 
 import sys,time,random
+
+def print_slow(str):
+    """
+    Prints the text slowly letter by letter
+    """
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
 
 def wishlist():
     """
     This function creates wishlists for the user and writes their wishes, aswell as writes it out for them.
     """
-    answer = input('Skriv "1" om du vill skapa en ny önskelista.\nSkriv "2" om du vill läsa upp en önskelista.\n')
+
+    answer = input('\nSkriv "1" om du vill skapa en ny önskelista.\nSkriv "2" om du vill läsa upp en önskelista. \nSKriv "3" om du vill lägg till nytt på en gammal önskelista. \n')
 
     if answer == "1":
 
         file_name = input("Vad vill du kalla önskelistan?\n")
         file_name = file_name + ".txt"
-        with open(file_name, "w", encoding="UTF-8") as y:
+        with open(file_name, "w", encoding="UTF-8") as x:
 
             child = input("Vad heter barnet: ")
-            y.write(f"Barnets namn: {child}\n")
+            x.write(f"Barnets namn: {child}\n\n")
 
-            y.write("---ÖNSKELISTA---\n")
+            x.write("---ÖNSKELISTA---\n")
 
             
             print("Skriv in det du önskar dig, skriv # när du vill avsluta.\n")
@@ -28,35 +40,81 @@ def wishlist():
                 if wish == "#":
                     break
                 else:
-                    y.write(f"{wish}\n")
-                    
-            time.sleep(1)
+                    x.write(f"{wish}\n")
 
-            def print_slow(str):
-                for letter in str:
-                    sys.stdout.write(letter)
-                    sys.stdout.flush()
-                    time.sleep(0.1)
-
-            print_slow("\nSkapar din önskelista...") 
+            time.sleep(0.5)
+            print_slow("\nSkapar din önskelista...\n\n") 
 
     elif answer == "2":
 
         file_name = input("Vilken önskelista vill du öppna?\n")
         file_name = file_name + ".txt"
 
-        time.sleep(1)
+        time.sleep(0.5)
+        print_slow("\nLoading...\n\n") 
 
-        def print_slow(str):
-            for letter in str:
-                sys.stdout.write(letter)
-                sys.stdout.flush()
-                time.sleep(0.1)
-
-        print_slow("\nLoading...") 
-        
         with open(file_name, "r", encoding="UTF-8") as x:
-            for line in x:
-                print(line)
+            readline=x.read().splitlines()
+            print(readline)
 
-wishlist()
+    elif answer == "2":
+
+        file_name = input("Vilken önskelista vill du öppna?\n")
+        file_name = file_name + ".txt"
+
+        time.sleep(0.5)
+        print_slow("\nLoading...\n\n") 
+
+        with open(file_name, "r", encoding="UTF-8") as x:
+            readline=x.read().splitlines()
+            print(readline)
+        
+        with open("kolbarn.txt", "a", encoding="UTF-8") as x:  
+            print("Skriv in det som ska läggas till, skriv # när du vill avsluta.\n")
+            while True:
+                wish = input("Önskemål: ")
+                if wish == "#":
+                    break
+                else:
+                    x.write(f"{wish}\n")  
+        print_slow("Uppdaterar din önskelist...")
+
+def naughtylist():
+    """
+    This function allows the user to read the naught list or add new children to the naughty list
+    """
+    answer = input('\nSkriv "1" om du vill läsa upp listan av kolbarn.\nSkriv "2" om du vill lägga till flera barn på listan av kolbarn.\n')
+
+    if answer == "1":
+        with open("kolbarn.txt", "r", encoding="UTF-8") as x:
+            
+            time.sleep(0.5)
+            print_slow("Loading...")
+            
+            readline=x.read().splitlines()
+            print(readline)
+            
+
+    elif answer == "2":
+        with open("kolbarn.txt", "r", encoding="UTF-8") as x:
+            readline=x.read().splitlines()
+            print(readline)
+
+        with open("kolbarn.txt", "a", encoding="UTF-8") as x:  
+            print("Skriv in det barn som ska läggas till, skriv # när du vill avsluta.\n")
+            while True:
+                name = input("Namn: ")
+                if name == "#":
+                    break
+                else:
+                    x.write(f"{name}\n")     
+            
+            time.sleep(0.5)
+            print_slow("Uppdaterar kolbarnslista...")
+
+answer = input('Vad vill du göra? \nSkriv "1" om du vill göra ny, läsa upp eller skriva på en gammal önskelista. \nSkriv "2" om du vill läsa upp eller lägga till barn på kolbarnslistan\n')
+
+if answer == "1":
+    wishlist()
+elif answer == "2":
+    naughtylist()
